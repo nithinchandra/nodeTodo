a3221bc0f0de9a7d220c2e0dcbd19047663eb670
nithinchandra <nithin@provenlogic.net>
2017-06-17 15:00:53 +0530

    errorfix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/Todo');
+//mongoose.connect('mongodb://localhost:27017/Todo');
+mongoose.connect('mongodb://nithin:nchandra@ds127962.mlab.com:27962/todos');
 
 
 module.exports = {mongoose}
