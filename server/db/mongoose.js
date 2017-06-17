b165f00b96c9788a393b117b910059491f90f1d7
nithinchandra <nithin@provenlogic.net>
2017-06-17 12:27:28 +0530

    'errof_fix'

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/Todo');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/Todo');
 
 module.exports = {mongoose}
